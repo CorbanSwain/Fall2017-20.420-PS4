09888cc3252e37e3275bb2d0aeb4e39b60b6d061
CorbanSwain <CorbanSwain@gmail.com>
2017-11-02 02:11:25 -0500

    Figure 1 and 2, Re FUnctionalizing Script

--- swain_corban_jones.m
+++ swain_corban_jones.m
@@ -14,11 +14,12 @@ function swain_corban_jones
     function main
         cleanup;
         close all;
-        figures = {fig1A_protocol, fig1B_protocol};
+        figures = {fig1, fig2};
         corban_figure_defaults;
         for i = 1:length(figures)
+            fprintf('Running Figure %2d\n',i);
             fh = makefigure(figures{i});
-            savefig(fh);
+%             savefig(fh);
         end
     end
 
@@ -32,7 +33,7 @@ k5 = 1e-2;  % [1/nM/s] - Conversion of mIIa to IIa by Va-Xa
 k6 = 0.1;   % [1/nM/s] - on-rate for rapidly formed complexes
 k7 = 1e-2;  % [1/nM/s] - on-rate for the VIIIa-IXa complex
 k8 = 0.4;   % [1/nM/s] - on-rate for the Va-Xa complex
-k9 = 5e-3;  % [1/s]   - off-rate for VIIIa-Ixa
+k9 = 5e-3;  % [1/s]   - off-rate for VIIIa-IXa
 k10 = 0.4;  % [1/s]   - off-rate for Va-Xa complex
 k11 = 0.3;  % [1/s]   - Vmax for activation of IX by TF-VIIIa
 k12 = 1.15; % [1/s]   - Vmax for activation of X by TF-VIIIa
@@ -106,63 +107,130 @@ ind.Va = 17;
 ind.VIIIa = 18;
 
 %% Useful Calculations
-thromb_form = @(y) y(:, ind.mIIa) * 1.2 + y(:, ind.IIa);
-thromb_percent = @(y, II_0) thromb_form(y) / II_0 * 100;
+col_sum = @(X,cols) sum(X(:,cols),2);
+thromb_activity = @(y) sum(y(:,[ind.mIIa, ind.IIa]) .* [1.2, 1], 2);
+thromb_percent = @(y, II_0) thromb_activity(y) / II_0 * 100;
+Xa_activity = @(y) col_sum(y, [ind.Xa, ind.Va_Xa, ind.Va_Xa_II]);
+Xa_percent = @(y, X_0) Xa_activity(y) / X_0 * 100;
+IXa_activity = @(y) col_sum(y, [ind.IXa, ind.VIIIa_IXa, ind.VIIIa_IXa_X]);
+IXa_percent = @(y, IX_0) IXa_activity(y) / IX_0 * 100;
 odesim = @(y0, p) ode15s(@odefun, tspan, y0, odeopts, p);
 
-%% Figures
-    function figspec = fig1A_protocol
-        p = p_original;
-        y0 = y0_original;
-        [t, y] = odesim(y0, p);
-        figspec.n = 1;
-        figspec.title = '1A-Thrombin Timecourse, Model Validation';
-        figspec.position = [3 640 606 324];
-        figspec.x = t;
-        figspec.y = thromb_percent(y, y0(ind.II));
-        figspec.xlabel = 'Time (seconds)';
-        figspec.ylabel = '% Thrombin Formation';
-        figspec.xlim = [0 250];
-        figspec.ylim = [0 120];
-        figspec.legend = {'Initial Model'};
-        figspec.legend_loc = 'northwest';
+    function ps_out = plotdefaults(ps_in)
+        ps_in.xlim = [0 250];
+        ps_in.xlabel = 'Time (s)';
+        ps_out = ps_in;
     end
 
-    function figspec = fig1B_protocol
+    function [t, y, y0, p] = sim_from_maps(initial_maps, param_maps)
+        n = length(initial_maps);
+        if n ~= length(param_maps)
+            error('Initial and param maps must be of the same size');
+        end
+        t = cell(n, 1); y = t; y0 = t; p = t;
+        for i = 1:n
+            y0{i} = y0_original;
+            p{i} = p_original;
+            im = initial_maps{i};
+            if size(im,1) > 0
+                y0{i}(im(:,1)) = im(:, 2);
+            end
+            pm = param_maps{i};
+            if size(pm,1) > 0 
+                p{i}(pm(:,1)) = pm(:,2);
+            end
+            [t{i}, y{i}] = odesim(y0{i}, p{i});
+        end
+    end
+
+%% Figures
+    function fs = fig1
         ntrials = 3;
-        y0 = y0_original;
-        map = cell(ntrials,1);
-        % Trial 0: No Change
-        map{1} = [];
-        % Trial 1: k7 = 1e6 1/M/s,  k9 = 5e-4 1/s
-        map{2} = [...
+        initial_map = cell(1, ntrials);
+        % Alternate 1: k7 = 1e6 1/M/s,  k9 = 5e-4 1/s
+        param_map{2} = [...
             7, 1e6 * 1e-9; ...
             9, 5e-4];
-        % Trial 2: k8 = 4e7 1/M/s, k10 = 4e-2 1/s
-        map{3} = [...
+        % Alternate 3: k8 = 4e7 1/M/s, k10 = 4e-2 1/s
+        param_map{3} = [...
             8, 4e7 * 1e-9; ...
-            10, 4e-2];
-        for i=1:ntrials
-            p = p_original;
-            m = map{i};
-            if size(m,1) > 0 
-                p(m(:,1)) = m(:,2);
+            10, 4e-2]; 
+        [t, y, y0] = sim_from_maps(initial_map, param_map);
+        
+        function ps = plotA
+            ps.x = t{1};
+            ps.y = thromb_percent(y{1}, y0{1}(ind.II));
+            ps = plotdefaults(ps);
+            ps.ylabel = '% Thrombin Formation';
+            ps.ylim = [0 120];
+            ps.legend = {'Initial Model'};
+            ps.legend_loc = 'northwest';
+        end
+
+        function ps = plotB
+            ps.x = t;
+            for i = 1:ntrials
+                ps.y{i} = thromb_activity(y{i}) ./ 1e3;
             end
-            [t{i}, y{i}] = odesim(y0, p);
+            ps = plotdefaults(ps);
+            ps.ylabel = 'Thrombin Formation (\muM)';
+            ps.ylim = 'auto';
+            ps.legend = {'Initial Model', 'Alternate 1', ...
+                'Alternate 2'};
+            ps.legend_loc = 'northwest';
         end
-        figspec.n = 2;
-        figspec.title = '1B-Thrombin Timecourse, Various Params';
-        figspec.position = [610 633 565 322];
-        figspec.x = t;
-        for i = 1:ntrials
-            figspec.y{i} = thromb_form(y{i}) ./ 1e3;
+
+        fs.n = 1;
+        fs.title = 'Thrombin Timecourse, Model Validation';
+        fs.position = [3 384 473 571];
+        fs.plots = {plotA, plotB};
+        fs.sub = [2, 1];
+    end
+
+    function fs = fig2
+        ntrials = 2;
+        initial_map = cell(1, ntrials);
+        % Alternate 1: No Degradation of VIIIa-IXa
+        param_map{2} = [...
+            9, 0, ...
+            20, 0];
+        [t, y, y0] = sim_from_maps(initial_map, param_map);
+        
+        ps_templ.legend = {'Initial Model','Stable VIIIa-IXa'};
+        ps_templ.legend_loc = 'northwest';
+        ps_templ.x = t;
+        ps_templ = plotdefaults(ps_templ);
+        
+        function ps = plotA
+            ps = ps_templ;
+            for i = 1:ntrials
+                ps.y{i} = thromb_percent(y{i}, y0{i}(ind.II)); 
+            end
+            ps.ylabel = '% Thrombin Formation';
+            ps.ylim = [0 120];
         end
-        figspec.xlabel = 'Time (seconds)';
-        figspec.ylabel = 'Thrombin Formation (\muM)';
-        figspec.xlim = [0 250];
-        figspec.ylim = 'auto';
-        figspec.legend = {'Initial Model', 'Trial 1', 'Trial 2'};
-        figspec.legend_loc = 'northwest';
+        function ps = plotB
+            ps = ps_templ;
+            for i = 1:ntrials
+                ps.y{i} = Xa_percent(y{i}, y0{i}(ind.X)); 
+            end
+            ps.ylabel = '% Xa Formation';
+            ps.ylim = [0 100];
+        end
+        function ps = plotC
+            ps = ps_templ;
+            for i = 1:ntrials
+                ps.y{i} = IXa_percent(y{i}, y0{i}(ind.IX)); 
+            end
+            ps.ylabel = '% IXa Formation';
+            ps.ylim = [0 70];
+        end
+        
+        fs.n = 2;
+        fs.title = 'Effecs of Stable VIIIa-IXa';
+        fs.position = [478 161 447 794];
+        fs.plots = {plotA, plotB, plotC};
+        fs.sub = [3, 1];
     end
 
 main;
@@ -174,28 +242,36 @@ if isfield(fs, 'position')
 else
     fighand = setupfig(fs.n, fs.title);
 end
-if iscell(fs.x) && iscell(fs.y)
-    n = length(fs.x);
-    if  n ~= length(fs.y)
+for i = 1:length(fs.plots)
+    subplot(fs.sub(1), fs.sub(2), i);
+    hold on;
+    makeplot(fs.plots{i});
+end
+end
+
+function makeplot(ps)
+if iscell(ps.x) && iscell(ps.y)
+    n = length(ps.x);
+    if  n ~= length(ps.y)
         error('x and y cell arrays have mismatched dimensions.');
     end
     for i = 1:n
-        plot(fs.x{i}, fs.y{i});
+        plot(ps.x{i}, ps.y{i});
     end
 else
-    if iscell(fs.x) || iscell(fs.y)
+    if iscell(ps.x) || iscell(ps.y)
         error('x and y values must both be matrices or cell arrays.');
     end
-    plot(fs.x, fs.y);
+    plot(ps.x, ps.y);
 end
-xlabel(fs.xlabel);
-ylabel(fs.ylabel);
-xlim(fs.xlim);
-ylim(fs.ylim);
-leg = legend(fs.legend);
+xlabel(ps.xlabel);
+ylabel(ps.ylabel);
+xlim(ps.xlim);
+ylim(ps.ylim);
+leg = legend(ps.legend);
 legend('boxoff');
-if isfield(fs, 'legend_loc')
-    leg.Location = fs.legend_loc;
+if isfield(ps, 'legend_loc')
+    leg.Location = ps.legend_loc;
 end
 end
 
